ae6ec96824f96b660534e42263bd5665d0859608
Cody Clark <opuseleven@protonmail.com>
2022-06-10 12:06:20 -0500

    Added Links to Toolbar

--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -1,10 +1,25 @@
+import Link from 'next/link';
+
 const Toolbar: React.FC = () => {
   return (
     <div>
+
+      <Link href='/'>
       <button>Home</button>
-      <button>About Me</button>
-      <button>Projects</button>
-      <button>GitHub</button>
+      </Link>
+
+      <Link href='/about'>
+        <button>About Me</button>
+      </Link>
+
+      <Link href='/projects'>
+        <button>Projects</button>
+      </Link>
+
+      <a href='https://github.com/opuseleven'>
+        <button>GitHub</button>
+      </a>
+
     </div>
   )
 }
