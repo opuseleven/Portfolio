2b3cb1002e680ff7cb65f71eac48b5c3fb33a08d
Cody Clark <opuseleven@protonmail.com>
2022-06-10 15:08:30 -0500

    Added ProjectSelector to Projects page.

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,7 +1,20 @@
 import type { NextPage } from 'next';
-import { Banner, Toolbar } from '../components';
+import { Banner, Toolbar, ProjectSelector } from '../components';
+import { Project } from '../types';
+import { useState, useEffect } from 'react';
 
 const Projects: NextPage = () => {
+
+  const projectData = require('../data/projects.json');
+
+  const [projects, setProjects] = useState<Project[]>(projectData);
+  const [selectedProject, setSelectedProject] = useState<Project>(projects[0]);
+  const [selectedId, setSelectedId] = useState(selectedProject.id);
+
+  useEffect(() => {
+    setSelectedProject(projects[selectedId - 1]);
+  }, [selectedId])
+
 
   return (
     <div>
@@ -12,6 +25,11 @@ const Projects: NextPage = () => {
 
       <h3>Projects</h3>
 
+      <div>
+
+        <ProjectSelector selectedId={selectedId} setSelectedId={setSelectedId} />
+
+      </div>
 
     </div>
   )
