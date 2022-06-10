8cf7f22515aef12c81f9b1aadc0244b7a6db98c5
Cody Clark <opuseleven@protonmail.com>
2022-06-10 11:58:24 -0500

    Added AboutMe page

---
+++ pages/about.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next';
+import { Banner, Toolbar } from '../components'
+
+const AboutMe: NextPage = () => {
+  return (
+    <div>
+
+      <Banner />
+
+      <Toolbar />
+
+      <h3>About Me</h3>
+      
+    </div>
+  )
+}
+export default AboutMe;
