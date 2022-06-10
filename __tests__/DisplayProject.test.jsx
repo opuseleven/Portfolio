8a7e97c81994a40c714a59920bc8db842fd947bd
Cody Clark <opuseleven@protonmail.com>
2022-06-10 18:14:35 -0500

    Added tests for DisplayProject component

---
+++ __tests__/DisplayProject.test.jsx
@@ -0,0 +1,17 @@
+import { DisplayProject } from '../components';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('DisplayProject component', () => {
+
+  const projects = require('../data/projects.json');
+
+  it('Component renders', () => {
+    render(<DisplayProject project={projects[0]} />);
+    expect(screen.getByRole('heading')).toHaveTextContent('Calculator goes here')
+  })
+  it('Changes with project prop', () => {
+    render(<DisplayProject project={projects[1]} />);
+    expect(screen.getByRole('heading')).toHaveTextContent('ToDoApp');
+  })
+})
