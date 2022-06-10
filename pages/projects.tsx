d91e6feef6707924284c087e54344dfe732dd071
Cody Clark <opuseleven@protonmail.com>
2022-06-10 12:07:45 -0500

    added Projects page

---
+++ pages/projects.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next';
+import { Banner, Toolbar } from '../components';
+
+const Projects: NextPage = () => {
+
+  return (
+    <div>
+
+      <Banner />
+
+      <Toolbar />
+
+      <h3>Projects</h3>
+
+
+    </div>
+  )
+}
+export default Projects;
