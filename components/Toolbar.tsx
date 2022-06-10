deb1dce6ae16ebcf31d472ad25266861427b3a8c
Cody Clark <opuseleven@protonmail.com>
2022-06-10 11:37:32 -0500

    Outlined Toolbar component

---
+++ components/Toolbar.tsx
@@ -0,0 +1,11 @@
+const Toolbar: React.FC = () => {
+  return (
+    <div>
+      <button>Home</button>
+      <button>About Me</button>
+      <button>Projects</button>
+      <button>GitHub</button>
+    </div>
+  )
+}
+export { Toolbar };
