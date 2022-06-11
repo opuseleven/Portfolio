1bd54506842bb68425aad328b1b19fb858bc020c
Cody Clark <opuseleven@protonmail.com>
2022-06-11 18:33:06 -0500

    Fixed test for About page

--- __tests__/about.test.jsx
+++ __tests__/about.test.jsx
@@ -1,10 +1,11 @@
 import AboutMe from '../pages/about';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 describe('AboutMe page', () => {
 
   it('Page renders', () => {
     render(<AboutMe />);
-    expect(screen.getAllByRole('heading')).toBeDefined();
+    expect(screen.getAllByRole('heading')[2]).toHaveTextContent('About Me');
   })
 })
