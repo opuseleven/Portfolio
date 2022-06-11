1938f515f16374e21a37e1664318ab4456412a53
Cody Clark <opuseleven@protonmail.com>
2022-06-11 18:17:02 -0500

    Fixed tests for Home page

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,10 +1,12 @@
 import { screen, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import Home from '../pages/index';
 
 describe('Home page', () => {
 
   it('Page renders', () => {
     render(<Home />);
-    expect(screen.getAllByRole('heading')).toBeDefined();
+    expect(screen.getAllByRole('heading')[0]).toHaveTextContent('Cody Clark');
+    expect(screen.getAllByRole('heading')[1]).toHaveTextContent('Development Portfolio');
   })
 })
