ba6772accb29d4de1e5a69fce020dcfb03f802e6
Cody Clark <opuseleven@protonmail.com>
2022-06-11 18:35:41 -0500

    Fixed test for Projects page

--- __tests__/projects.test.jsx
+++ __tests__/projects.test.jsx
@@ -1,10 +1,11 @@
 import Projects from '../pages/projects';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 describe('Projects page', () => {
 
   it('Page renders', () => {
     render(<Projects />);
-    expect(screen.getAllByRole('heading')).toBeDefined();
+    expect(screen.getAllByRole('heading')[2]).toHaveTextContent('Projects');
   })
 })
