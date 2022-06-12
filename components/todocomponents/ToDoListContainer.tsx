3d763e4256952d037b896cef6dd9a5e021435bec
Cody Clark <opuseleven@protonmail.com>
2022-06-11 19:17:36 -0500

    Outlined ToDoListContainer

--- components/todocomponents/ToDoListContainer.tsx
+++ components/todocomponents/ToDoListContainer.tsx
@@ -1,3 +1,7 @@
+import styles from '../../styles/ToDo.module.css';
+import { ToDoList } from '.';
+import { getSortedLists } from '../../services/todoservices';
+import { useState, useEffect } from 'react';
 import { ToDo } from '../../types';
 
 interface ToDoListContainerProps {
@@ -7,8 +11,23 @@ interface ToDoListContainerProps {
 
 const ToDoListContainer: React.FC<ToDoListContainerProps> = ({ arr, setArr }) => {
 
+  const [sortedLists, setSortedLists] = useState(getSortedLists(arr));
+
+  useEffect(() => {
+    sort();
+  }, [arr])
+
+  function sort() {
+    setSortedLists(getSortedLists(arr));
+  }
+
   return (
     <div>
+
+      <ToDoList todolist={sortedLists.todoArr} arr={arr} setArr={setArr} sort={sort} />
+
+      <ToDoList todolist={sortedLists.completedArr} arr={arr} setArr={setArr} sort={sort} />
+
     </div>
   )
 }
