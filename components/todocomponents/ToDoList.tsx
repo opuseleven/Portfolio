1a22ba80a28c3cee99aa17170dd3fabf7c579d87
Cody Clark <opuseleven@protonmail.com>
2022-06-11 20:45:16 -0500

    Added ToDoList component

---
+++ components/todocomponents/ToDoList.tsx
@@ -0,0 +1,33 @@
+import styles from '../../styles/ToDo.module.css';
+import { ToDoView } from '.';
+import { useState, useEffect } from 'react';
+import { ToDo } from '../../types';
+
+interface ToDoListProps {
+  todolist: ToDo[],
+  arr: ToDo[],
+  setArr: React.Dispatch<React.SetStateAction<ToDo[]>>,
+  sort: VoidFunction
+}
+
+const ToDoList: React.FC<ToDoListProps> = ({ todolist, arr, setArr, sort }) => {
+
+  const [displayedList, setDisplayedList] = useState(todolist);
+
+  useEffect(() => {
+    setDisplayedList(todolist);
+  }, [todolist])
+
+  return (
+    <div>
+      {
+        displayedList.map(t => (
+          <div key={t.id.toString()} className={styles.todolist}>
+            <ToDoView todo={t} arr={arr} setArr={setArr} sort={sort} />
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+export { ToDoList };
