572fc7c5b4078a91a0191119fe65e5306865fc7f
Cody Clark <opuseleven@protonmail.com>
2022-06-11 20:47:10 -0500

    Added removeToDo service

---
+++ services/todoservices/removeToDo.ts
@@ -0,0 +1,14 @@
+import { ToDo } from '../../types';
+
+function removeToDo(todo: ToDo, arr: ToDo[], setArr: React.Dispatch<React.SetStateAction<ToDo[]>>) {
+
+  let newArr = [...arr];
+
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i].id == todo.id) {
+      newArr.splice(i, 1);
+    }
+  }
+  setArr(newArr);
+}
+export { removeToDo };
