9b1e587667c398727208fc59c79864aae127742f
Cody Clark <opuseleven@protonmail.com>
2022-06-11 20:47:43 -0500

    Exported components

--- components/todocomponents/index.js
+++ components/todocomponents/index.js
@@ -1,2 +1,6 @@
 export * from './AddToDoButton';
 export * from './ToDoListContainer';
+export * from './ToDoList';
+export * from './ToDoView';
+export * from './CompletedButton';
+export * from './DeleteButton';
