e59c26c07887d6852080a9738964b0eb96ac9527
Cody Clark <opuseleven@protonmail.com>
2022-06-11 19:08:06 -0500

    Outlined ToDo application

--- components/ToDoApp.tsx
+++ components/ToDoApp.tsx
@@ -1,8 +1,30 @@
+import { useState } from 'react';
+import { ToDo } from '../types';
+import { AddToDoButton, ToDoListContainer } from '../components/todocomponents';
+
 const ToDoApp: React.FC = () => {
+
+  const defaultItem = {
+    id: 1,
+    name: '',
+    completed: false
+  }
+
+  const defaultList = [defaultItem];
+
+  const [arr, setArr] = useState<ToDo[]>(defaultList);
 
   return (
     <div>
       <h1>ToDoApp</h1>
+
+      <div>
+
+        <AddToDoButton arr={arr} setArr={setArr} />
+
+        <ToDoListContainer arr={arr} setArr={setArr} />
+
+      </div>
     </div>
   )
 }
