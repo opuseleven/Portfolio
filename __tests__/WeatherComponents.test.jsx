f55fb4d2160af7bc90e4183a841e1817eac38697
Cody Clark <opuseleven@protonmail.com>
2022-06-13 15:51:42 -0500

    Added all the unit tests for WeatherApp components

--- __tests__/WeatherComponents.test.jsx
+++ __tests__/WeatherComponents.test.jsx
@@ -1,6 +1,39 @@
 import { CityDisplay, Info, RenderImage, WeatherDisplay, WeatherFooter } from '../components/weathercomponents';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 describe('All WeatherApp component tests', () => {
 
+  const testdata = require('../data/weathertestdata.json');
+
+  it('CityDisplay component renders and passes data correctly', () => {
+    render(<CityDisplay data={testdata} />);
+    expect(screen.getByRole('heading')).toHaveTextContent('Nashville');
+  })
+
+  it('Info component renders', () => {
+    render(<Info />);
+    expect(screen.getByRole('heading')).toHaveTextContent('Search by city name.');
+  })
+
+  it('RenderImage component renders', async () => {
+    render(<RenderImage image={'/../public/weather-images/cloudy.png'} />);
+    await(screen.getByRole('img'));
+    expect(screen.getByRole('img')).toBeDefined();
+  })
+
+  it('WeatherDisplay component renders and passes data', () => {
+    render(<WeatherDisplay data={testdata} />);
+    const headings = screen.getAllByRole('heading');
+    expect(headings[1]).toHaveTextContent('Clear');
+    expect(headings[3]).toHaveTextContent('Clear');
+    expect(headings[5]).toHaveTextContent('Clear');
+  })
+
+  it('WeatherFooter component renders', () => {
+    render(<WeatherFooter />);
+    const headings = screen.getAllByRole('heading');
+    expect(headings[0]).toHaveTextContent('Data provided by:');
+    expect(headings[1]).toHaveTextContent('openweathermap.org');
+  })
 })
