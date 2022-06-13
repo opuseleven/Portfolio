7bdfbebbfe91c7c7902d642e5b03f78da10c1d33
Cody Clark <opuseleven@protonmail.com>
2022-06-13 13:05:42 -0500

    Added Day interface

---
+++ types/Day.ts
@@ -0,0 +1,9 @@
+export interface Day {
+  weather: string,
+  temp: number,
+  humidity: string,
+  weatherDescription: string,
+  precipitation: number,
+  weatherId: number,
+  weatherImage: string
+}
