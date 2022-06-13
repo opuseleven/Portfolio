e6bc4be77c27264093e8afb41ac9dcbfb918b71d
Cody Clark <opuseleven@protonmail.com>
2022-06-13 13:43:10 -0500

    Added file for WeatherApp component tests

---
+++ __tests__/WeatherComponents.test.jsx
@@ -0,0 +1,6 @@
+import { CityDisplay, Info, RenderImage, WeatherDisplay, WeatherFooter } from '../components/weathercomponents';
+import { render, screen } from '@testing-library/react';
+
+describe('All WeatherApp component tests', () => {
+
+})
