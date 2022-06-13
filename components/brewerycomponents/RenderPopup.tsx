2790d0c96fc709a010b72656283a0e2873d258c0
Cody Clark <opuseleven@protonmail.com>
2022-06-12 21:00:43 -0500

    Added RenderPopup component

---
+++ components/brewerycomponents/RenderPopup.tsx
@@ -0,0 +1,40 @@
+import { Popup } from 'react-map-gl';
+import { useState, useEffect } from 'react';
+import { PopupContents } from './';
+import { Brewery } from '../../types';
+
+interface RenderPopupProps {
+  brewery: Brewery,
+  showPopup: boolean,
+  setShowPopup: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const RenderPopup: React.FC<RenderPopupProps> = ({ brewery, showPopup, setShowPopup }) => {
+
+  const [popup, setPopup] = useState(showPopup);
+
+  useEffect(() => {
+    setPopup(showPopup);
+  }, [brewery, showPopup])
+
+  function handleClose() {
+    setShowPopup(false);
+  }
+
+  return (
+    <div role="popup">
+      {
+        popup && (
+          <Popup
+            longitude={brewery.longitude}
+            latitude={brewery.latitude}
+            anchor="top"
+            onClose={() => handleClose}
+          >
+            <PopupContents brewery={brewery} />
+          </Popup>)
+      }
+    </div>
+  )
+}
+export { RenderPopup };
