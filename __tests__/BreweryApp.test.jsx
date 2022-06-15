8dcc64575d47c3d2813139d185f22f55428f5fb5
Cody Clark <opuseleven@protonmail.com>
2022-06-15 14:43:36 -0500

    Added tests for BreweryApp component

---
+++ __tests__/BreweryApp.test.jsx
@@ -0,0 +1,28 @@
+import { BreweryApp } from '../components';
+import { render, screen, act, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('BreweryApp component', () => {
+
+  it('Component renders', () => {
+    render(<BreweryApp />);
+    const buttons = screen.getAllByRole('button');
+    expect(buttons[0]).toHaveTextContent('Submit');
+    expect(buttons[1]).toHaveTextContent('Filter');
+    expect(buttons[2]).toHaveTextContent('Show Map');
+  })
+
+  it('UI functions to run app', async () => {
+    render(<BreweryApp />);
+    const buttons = screen.getAllByRole('button');
+    const textbox = screen.getByRole('textbox');
+    await act(async () => {
+      fireEvent.change(textbox, {target: {value: 'nashville'}});
+      fireEvent.click(buttons[0]);
+      await new Promise(r => setTimeout(r, 3000));
+      fireEvent.click(buttons[2]);
+      await new Promise(r => setTimeout(r, 1000));
+    })
+    expect(screen.getByRole('map')).toBeDefined();
+  })
+})
