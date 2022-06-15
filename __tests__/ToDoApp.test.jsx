0e42a9d9279f373d54399c8f6673dd960ee2a652
Cody Clark <opuseleven@protonmail.com>
2022-06-15 15:46:50 -0500

    Added additional tests for ToDoApp component

--- __tests__/ToDoApp.test.jsx
+++ __tests__/ToDoApp.test.jsx
@@ -8,30 +8,101 @@ describe('ToDoApp component', () => {
     expect(screen.getByRole('textbox')).toBeDefined();
   })
 
-  it('Add Todo button functions', () => {
+  it('Textbox updates', () => {
     render(<ToDoApp />);
+    const headings = screen.getAllByRole('textbox');
     act(() => {
-      fireEvent.click(screen.getByText('+'));
+      fireEvent.change(headings[0], {target: {value: 'one'}});
     })
-    expect(screen.getAllByRole('textbox').length).toBe(2);
-    act(() => {
-      fireEvent.click(screen.getByText('+'));
-    })
-    expect(screen.getAllByRole('textbox').length).toBe(3);
+    expect(headings[0].value).toBe('one');
   })
 
-  it('DeleteButton functions', () => {
+  it('UI allows adding of list item', () => {
     render(<ToDoApp />);
+    let headings = screen.getAllByRole('textbox');
+    const buttons = screen.getAllByRole('button');
     act(() => {
-      fireEvent.click(screen.getByText('+'));
+      fireEvent.change(headings[0], {target: {value: 'one'}});
+      fireEvent.click(buttons[0]);
     })
+    headings = screen.getAllByRole('textbox');
     act(() => {
-      fireEvent.click(screen.getByText('+'));
+      fireEvent.change(headings[1], {target: {value: 'two'}});
     })
-    expect(screen.getAllByRole('textbox').length).toBe(3);
+    expect(headings[0].value).toBe('one');
+    expect(headings[1].value).toBe('two');
     act(() => {
-      fireEvent.click(screen.getAllByText('x')[1]);
+      fireEvent.click(buttons[0]);
     })
-    expect(screen.getAllByRole('textbox').length).toBe(2);
+    headings = screen.getAllByRole('textbox');
+    act(() => {
+      fireEvent.change(headings[2], {target: {value: 'three'}});
+    })
+    expect(headings[2].value).toBe('three');
+    expect(headings.length).toBe(3);
+  })
+
+  it('UI allows removing of list item', () => {
+    render(<ToDoApp />);
+    let headings = screen.getAllByRole('textbox');
+    let buttons = screen.getAllByRole('button');
+    act(() => {
+      fireEvent.change(headings[0], {target: {value: 'one'}});
+      fireEvent.click(buttons[0]);
+    })
+    headings = screen.getAllByRole('textbox');
+    act(() => {
+      fireEvent.change(headings[1], {target: {value: 'two'}});
+      fireEvent.click(buttons[0]);
+    })
+    headings = screen.getAllByRole('textbox');
+    act(() => {
+      fireEvent.change(headings[2], {target: {value: 'three'}});
+    })
+    expect(headings[0].value).toBe('one');
+    expect(headings[1].value).toBe('two');
+    expect(headings[2].value).toBe('three');
+    expect(headings.length).toBe(3);
+    buttons = screen.getAllByRole('button');
+    act(() => {
+      fireEvent.click(buttons[2]);
+    })
+    headings = screen.getAllByRole('textbox');
+    expect(headings.length).toBe(2);
+    expect(headings[0].value).toBe('one');
+    expect(headings[1].value).toBe('three');
+  })
+
+  it('Clicking checkbox sorts list by completed', () => {
+    render(<ToDoApp />);
+    let headings = screen.getAllByRole('textbox');
+    let buttons = screen.getAllByRole('button');
+    act(() => {
+      fireEvent.change(headings[0], {target: {value: 'one'}});
+      fireEvent.click(buttons[0]);
+    })
+    headings = screen.getAllByRole('textbox');
+    act(() => {
+      fireEvent.change(headings[1], {target: {value: 'two'}});
+      fireEvent.click(buttons[0]);
+    })
+    headings = screen.getAllByRole('textbox');
+    act(() => {
+      fireEvent.change(headings[2], {target: {value: 'three'}});
+    })
+    expect(headings[0].value).toBe('one');
+    expect(headings[1].value).toBe('two');
+    expect(headings[2].value).toBe('three');
+    expect(headings.length).toBe(3);
+    let checkboxes = screen.getAllByRole('checkbox');
+    act(async () => {
+      fireEvent.click(checkboxes[1]);
+      await new Promise(r => setTimeout(r, 1000));
+    })
+    headings = screen.getAllByRole('textbox');
+    expect(headings.length).toBe(3);
+    expect(headings[0].value).toBe('one');
+    expect(headings[1].value).toBe('three');
+    expect(headings[2].value).toBe('two');
   })
 })
