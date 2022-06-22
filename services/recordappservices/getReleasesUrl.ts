259bf083d8eedb910425dc8b5d7dd13fccdaa41e
Cody Clark <opuseleven@protonmail.com>
2022-06-21 21:02:06 -0500

    Added services

---
+++ services/recordappservices/getReleasesUrl.ts
@@ -0,0 +1,24 @@
+import { Artist } from '../../types';
+import axios from 'axios';
+
+function getReleasesUrl(artist: Artist, setReleasesUrl: React.Dispatch<React.SetStateAction<string>>) {
+
+  const url = 'https://api.discogs.com/artists/' + artist.id.toString();
+  const token = process.env.TOKEN;
+
+  axios
+    .request({
+      url: url,
+      method: 'get',
+      responseType: 'json',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Discogs token=' + token
+      }
+    })
+    .then((res) => setReleasesUrl(res.data.releases_url))
+    .catch((err) => {
+      console.log(err);
+    });
+}
+export { getReleasesUrl };
---
+++ services/recordappservices/index.js
@@ -0,0 +1,5 @@
+export * from './filterByArtists';
+export * from './getArtist';
+export * from './filterByHasType';
+export * from './artistMatch';
+export * from './getReleasesUrl';
