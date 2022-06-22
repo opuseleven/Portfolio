ede77089c73601cc4e3ac32272d8f6fcf9b7939b
Cody Clark <opuseleven@protonmail.com>
2022-06-22 18:13:14 -0500

    Added ResultsContainer component

---
+++ components/recordappcomponents/ResultsContainer.tsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+import {  Artist } from '../../types';
+import { ArtistDetails, ReleaseDetails } from '.';
+
+interface ResultsContainerProps {
+  artist: Artist,
+  releasesUrl: string
+}
+
+const ResultsContainer: React.FC<ResultsContainerProps> = ({ artist, releasesUrl }) => {
+
+  const [displayedArtist, setDisplayedArtist] = useState(artist);
+  const [url, setUrl] = useState(releasesUrl);
+
+  useEffect(() => {
+    setDisplayedArtist(artist);
+  }, [artist])
+
+  useEffect(() => {
+    setUrl(releasesUrl);
+  }, [releasesUrl])
+
+  return (
+    <div>
+      <div>
+        {
+          displayedArtist && (
+            <ArtistDetails artist={displayedArtist} />
+          )
+        }
+      </div>
+
+      <div>
+        {
+          url && (
+            <ReleaseDetails releasesUrl={url} />
+          )
+        }
+      </div>
+
+    </div>
+  )
+}
+export { ResultsContainer };
