dade2c9b3d5b452bb64f5bb89db42ecf4c180071
Cody Clark <opuseleven@protonmail.com>
2022-06-22 18:13:46 -0500

    Added ArtistDetails component

---
+++ components/recordappcomponents/ArtistDetails.tsx
@@ -0,0 +1,23 @@
+import { Artist } from '../../types';
+
+interface ArtistDetailsProps {
+  artist: Artist
+}
+
+const ArtistDetails: React.FC<ArtistDetailsProps> = ({ artist }) => {
+
+  return (
+    <div>
+      <div>
+        {
+          artist && (
+            <div>
+              <h3>{artist.title}</h3>
+            </div>
+          )
+        }
+      </div>
+    </div>
+  )
+}
+export { ArtistDetails };
