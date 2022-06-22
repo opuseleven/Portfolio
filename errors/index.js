5086765af6f266fbb2437622eb674f8be5c4f3a2
Cody Clark <opuseleven@protonmail.com>
2022-06-22 18:21:25 -0500

    Exported ReleasesNotFoundError

--- errors/index.js
+++ errors/index.js
@@ -2,3 +2,4 @@ export * from './ProjectNotFoundError';
 export * from './BreweryError';
 export * from './ArtistNotFoundError';
 export * from './RecordDataError';
+export * from './ReleasesNotFoundError';
