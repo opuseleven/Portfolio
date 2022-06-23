b1556191ae06218c7632b2425e82bf5bbb4a33a3
Cody Clark <opuseleven@protonmail.com>
2022-06-22 19:44:04 -0500

    Added RenderExtraArtist component

---
+++ components/recordappcomponents/RenderExtraArtist.tsx
@@ -0,0 +1,24 @@
+import { ExtraArtist } from '../../types';
+import { useState, useEffect } from 'react';
+
+interface RenderExtraArtistProps {
+  extraArtist: ExtraArtist
+}
+
+const RenderExtraArtist: React.FC<RenderExtraArtistProps> = ({ extraArtist }) => {
+
+  const [artist, setArtist] = useState(extraArtist);
+
+  useEffect(() => {
+    setArtist(extraArtist);
+  }, [extraArtist])
+
+  return (
+    <div>
+      <ul>
+        <li><h6>{artist.role} - {artist.name}</h6></li>
+      </ul>
+    </div>
+  )
+}
+export { RenderExtraArtist };
