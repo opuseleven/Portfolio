2bb613abe50a9a27d0a458ed21e595ac0fc01dd7
Cody Clark <opuseleven@protonmail.com>
2022-06-22 19:44:21 -0500

    Added RenderTrack component

---
+++ components/recordappcomponents/RenderTrack.tsx
@@ -0,0 +1,39 @@
+import { Track } from '../../types';
+import { RenderExtraArtist } from '.';
+import { useState, useEffect } from 'react';
+
+interface RenderTrackProps {
+  track: Track
+}
+
+const RenderTrack: React.FC<RenderTrackProps> = ({ track }) => {
+
+  const [displayedTrack, setDisplayedTrack] = useState(track);
+  const [extraArtists, setExtraArtists] = useState(track.extraartists);
+
+  useEffect(() => {
+    setDisplayedTrack(track);
+    setExtraArtists(track.extraartists);
+  }, [track])
+
+  return (
+    <div>
+      <h6>{displayedTrack.position}. {displayedTrack.title}</h6>
+      <ul>
+        <li><h6>Track length: {displayedTrack.duration}</h6></li>
+      </ul>
+      <div>
+        {
+          extraArtists && (
+            extraArtists.map(a => (
+              <div key={a.id.toString()}>
+                <RenderExtraArtist extraArtist={a} />
+              </div>
+            ))
+          )
+        }
+      </div>
+    </div>
+  )
+}
+export { RenderTrack };
