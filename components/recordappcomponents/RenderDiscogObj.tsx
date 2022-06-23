65f29db2d0bcc8cf96c1a996e625a3f6dec67338
Cody Clark <opuseleven@protonmail.com>
2022-06-22 19:43:46 -0500

    Added RenderDiscogObj component

---
+++ components/recordappcomponents/RenderDiscogObj.tsx
@@ -0,0 +1,39 @@
+import { DiscogObj } from '../../types';
+import { useState, useEffect } from 'react';
+import styles from '../styles/Components.module.css';
+
+interface RenderDiscogObjProps {
+  obj: DiscogObj
+}
+
+const RenderDiscogObj: React.FC<RenderDiscogObjProps> = ({ obj }) => {
+
+  const [displayedObj, setDisplayedObj] = useState(obj);
+
+  useEffect(() => {
+    setDisplayedObj(obj);
+  }, [obj])
+
+  return (
+    <div className={styles.objcontainer}>
+      <ul>
+        <li><h5>{displayedObj.name}</h5></li>
+        <div>
+          {
+            displayedObj.entity_type_name && (
+              <li><h5>Type: {displayedObj.entity_type_name}</h5></li>
+            )
+          }
+        </div>
+        <div>
+          {
+            displayedObj.role && (
+              <li><h5>Role: {displayedObj.role}</h5></li>
+            )
+          }
+        </div>
+      </ul>
+    </div>
+  )
+}
+export { RenderDiscogObj };
