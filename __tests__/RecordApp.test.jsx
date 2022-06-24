ce3922a9c64aad728d4979dd986ebf92fa1ecade
Cody Clark <opuseleven@protonmail.com>
2022-06-24 12:16:31 -0500

    Added tests for RecordDataSearchApp component

---
+++ __tests__/RecordApp.test.jsx
@@ -0,0 +1,24 @@
+import { RecordDataSearchApp } from '../components';
+import { render, screen, act, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('RecordDataSearchApp component', () => {
+
+  it('Component renders', () => {
+    render(<RecordDataSearchApp />);
+    expect(screen.getAllByRole('heading')[0]).toHaveTextContent('RecordDataSearch');
+  })
+
+  it('UI functions to control application', async () => {
+    render(<RecordDataSearchApp />);
+    expect(screen.getAllByRole('heading')[0]).toHaveTextContent('RecordDataSearch');
+    act(() => {
+      fireEvent.change(screen.getByRole('textbox'), {target: {value: 'haunted like human'}});
+      fireEvent.click(screen.getByRole('button'));
+    })
+    await act(async () => {
+      await new Promise(r => setTimeout(r, 3000));
+    })
+    expect(screen.getAllByRole('heading')[1]).toHaveTextContent('Haunted Like Human');
+  })
+})
