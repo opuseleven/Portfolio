9b71523b8f30038a92d5388958d006f82d267d50
Cody Clark <opuseleven@protonmail.com>
2022-06-24 20:30:59 -0500

    Added tests for ResultsPageNav component

--- __tests__/RecordAppComponents.test.jsx
+++ __tests__/RecordAppComponents.test.jsx
@@ -1,5 +1,7 @@
 import { ArtistDetails, DisplayDetailedRelease, DisplayRelease, ReleaseDetails,
-  RenderDiscogObj, RenderExtraArtist, RenderTrack, ResultsContainer, SearchBar } from '../components/recordappcomponents';
+  RenderDiscogObj, RenderExtraArtist, RenderTrack, ResultsContainer, SearchBar,
+  ResultsPageNav } from '../components/recordappcomponents';
+import { defaultPagination } from '../types';
 import { render, screen, act, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
@@ -88,6 +90,37 @@ describe('All RecordDataSearchApp Component tests', () => {
   it('ResultsContainer component renders', () => {
     render(<ResultsContainer artist={artistData[0]} releasesUrl={'https://api.discogs.com/artists/7628210/releases'} />);
     expect(screen.getAllByRole('heading')[0]).toHaveTextContent('Haunted Like Human');
+  })
+
+  let page = 1;
+  function setPage(n) {
+    page = n;
+  }
+
+  it('ResultsPageNav component renders', () => {
+    render(<ResultsPageNav page={page} setPage={setPage} pagination={defaultPagination()} />);
+    expect(screen.getByRole('heading')).toHaveTextContent('1');
+  })
+
+  it('ResultsPageNav UI function to update page state var', async () => {
+    const testPagination = {
+      page: 1,
+      pages: 4,
+      per_page: 5,
+      item: 19,
+      urls: {
+        last: '',
+        next: ''
+      }
+    }
+    render(<ResultsPageNav page={page} setPage={setPage} pagination={testPagination} />);
+    expect(screen.getByRole('heading')).toHaveTextContent('1');
+    expect(screen.getByRole('button')).toHaveTextContent('Next page');
+    await act(async () => {
+      fireEvent.click(screen.getByRole('button'));
+      await new Promise(r => setTimeout(r, 1000));
+    })
+    expect(page).toBe(2);
   })
 
   it('SearchBar component renders', () => {
