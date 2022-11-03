63bd2b80fb43a9c01a16deb105d48313eb2da265
Cody Clark <opuseleven@protonmail.com>
2022-11-02 21:47:08 -0500

    Removed unused files from adding BrewerySearchApp submodule

--- services/breweryservices/filterByHasCoordinates.ts
+++
@@ -1,7 +0,0 @@
-import { Brewery } from '../../types';
-import { coordinateCheck } from '.';
-
-function filterByHasCoordinates(arr: Brewery[]) {
-  return arr.filter(b => coordinateCheck(b))
-}
-export { filterByHasCoordinates };
--- services/breweryservices/index.js
+++
@@ -1,6 +0,0 @@
-export * from './handleSearchClick';
-export * from './filterByType';
-export * from './typeFilterCheck';
-export * from './filterByHasCoordinates';
-export * from './coordinateCheck';
-export * from './getBrewery';
