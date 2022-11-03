53444001b6b4ee7d5e7eeb734c681ed14d449d0b
Cody Clark <opuseleven@protonmail.com>
2022-11-02 21:58:07 -0500

    Added App from WeatherApp module

--- components/WeatherApp.tsx
+++ components/WeatherApp.tsx
@@ -1,67 +1,12 @@
 import styles from '../styles/Weather.module.css';
-import { Info, WeatherFooter, WeatherDisplay, CityDisplay } from './weathercomponents';
-import { useField } from '../hooks';
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import { ApiData } from '../types';
+import { FC } from 'react'
+import App from './WeatherApp/pages';
 
-const WeatherApp: React.FC = () => {
-
-  const [citySearch, setCitySearch] = useState<string>("Nashville");
-  const tempData: ApiData = require('../data/weathertestdata.json');
-  const [data, setData] = useState<ApiData>(tempData);
-  const apiKey = process.env.WEATHERAPP_API_KEY;
-  const searchInput = useField('text');
-
-  function refreshData() {
-    const newUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + citySearch + '&cnt=3&appid=' + apiKey;
-    axios
-      .request({url: newUrl}).then((response) => setData(response.data));
-  }
-
-  function handleClick(e: any) {
-    e.preventDefault();
-    setCitySearch(searchInput.value);
-    refreshData();
-  }
-
-  useEffect(() => {
-    refreshData()
-  }, []);
+const WeatherApp: FC = () => {
 
   return (
-    <div className={styles.container}>
-      <div className={styles.main}>
-        <div className={styles.title}>
-          <h1>WeatherApp</h1>
-        </div>
-        <div className={styles.infocontainer}>
-          <Info />
-        </div>
-
-        <div className={styles.formcontainer}>
-          <form onSubmit={handleClick}>
-            <input {...searchInput} />
-            <button>Search</button>
-          </form>
-        </div>
-
-        <div className={styles.citydisplaycontainer}>
-          {data && (
-            <CityDisplay  data={data} />
-          )}
-        </div>
-
-        <div>
-          {data && (
-            <WeatherDisplay data={data} />
-            )
-          }
-        </div>
-      </div>
-      <div className={styles.footercontainer}>
-        <WeatherFooter />
-      </div>
+    <div>
+      <App />
     </div>
   )
 }
