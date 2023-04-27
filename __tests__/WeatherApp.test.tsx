c6eca8996e5ea6a40538ded0edd3fa8d17d8c0e0
Cody Clark <opuseleven@protonmail.com>
2023-04-27 17:08:48 -0500

    Changed WeatherApp tests to typescript

--- __tests__/WeatherApp.test.tsx
+++ __tests__/WeatherApp.test.tsx
@@ -9,19 +9,25 @@ describe('WeatherApp component', () => {
     const headings = screen.getAllByRole('heading');
     expect(headings[0]).toHaveTextContent('WeatherApp');
     expect(headings[1]).toHaveTextContent('Search by city name.');
-    expect(headings[2]).toHaveTextContent('Nashville');
+    expect(headings[2]).toHaveTextContent('Or search by zip code');
+    expect(headings[3]).toHaveTextContent('Nashville');
   })
 
   it('UI functions to control app', async () => {
     render(<WeatherApp />);
+    await act(async () => {
+      await new Promise(r => setTimeout(r, 1000));
+    })
     act(() => {
-      fireEvent.change(screen.getByRole('textbox'), {target: {value: 'nashville'}});
+      fireEvent.change(screen.getByRole('textbox'), {target: {value: 'walla walla'}});
+    })
+    act(() => {
+      fireEvent.click(screen.getByRole('button'));
     })
     await act(async () => {
-      fireEvent.click(screen.getByRole('button'));
-      await new Promise(r => setTimeout(r, 3000));
+      await new Promise(r => setTimeout(r, 2000));
     })
     const headings = screen.getAllByRole('heading');
-    expect(headings[2]).toHaveTextContent('Nashville');
+    expect(headings[3]).toHaveTextContent('Walla Walla');
   })
 })
